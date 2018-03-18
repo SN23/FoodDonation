03b713e46b6798cd96766861118aaf6ee2450239
Sukhjinder <nahal.snahal@gmail.com>
2018-03-18 16:52:06 +0400

    setup mainActivity

--- app/src/main/java/com/nahal/sukhjinder/fooddonation/MainActivity.java
+++ app/src/main/java/com/nahal/sukhjinder/fooddonation/MainActivity.java
@@ -1,13 +1,38 @@
 package com.nahal.sukhjinder.fooddonation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.nahal.sukhjinder.fooddonation.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mainBinding.donateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent donateIntent = new Intent(MainActivity.this, DonateActivity.class);
+                startActivity(donateIntent);
+            }
+        });
+
+        mainBinding.pickupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pickupIntent = new Intent(MainActivity.this, PickupActivity.class);
+                startActivity(pickupIntent);
+
+            }
+        });
     }
 }
