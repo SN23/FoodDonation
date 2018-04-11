5012d00108102797e5fe3e064a5c0a27117c3357
Sukhjinder Nahal <SN23@users.noreply.github.com>
2018-04-11 15:08:25 -0400

    created Donation Model and deleted view_models

--- app/src/main/java/com/nahal/sukhjinder/fooddonation/model/Donation.java
+++ app/src/main/java/com/nahal/sukhjinder/fooddonation/model/Donation.java
@@ -1,19 +1,96 @@
 package com.nahal.sukhjinder.fooddonation.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Sukhjinder on 3/19/18.
  */
 
-public class Donation {
+public class Donation implements Parcelable {
 
     private String donationStatus;
-    private String foodDonation;
+    private String foodDescription;
     private int foodPicture;
-    private String donationDate;
-    private boolean accepted;
+    private String pickupDate;
+    private String pickupTime;
 
     public Donation() {
 
     }
 
+    public String getDonationStatus() {
+        return donationStatus;
+    }
+
+    public void setDonationStatus(String donationStatus) {
+        this.donationStatus = donationStatus;
+    }
+
+    public String getFoodDescription() {
+        return foodDescription;
+    }
+
+    public void setFoodDescription(String foodDescription) {
+        this.foodDescription = foodDescription;
+    }
+
+    public int getFoodPicture() {
+        return foodPicture;
+    }
+
+    public void setFoodPicture(int foodPicture) {
+        this.foodPicture = foodPicture;
+    }
+
+    public String getPickupDate() {
+        return pickupDate;
+    }
+
+    public void setPickupDate(String pickupDate) {
+        this.pickupDate = pickupDate;
+    }
+
+    public String getPickupTime() {
+        return pickupTime;
+    }
+
+    public void setPickupTime(String pickupTime) {
+        this.pickupTime = pickupTime;
+    }
+
+    protected Donation(Parcel in) {
+        donationStatus = in.readString();
+        foodDescription = in.readString();
+        foodPicture = in.readInt();
+        pickupDate = in.readString();
+        pickupTime = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(donationStatus);
+        dest.writeString(foodDescription);
+        dest.writeInt(foodPicture);
+        dest.writeString(pickupDate);
+        dest.writeString(pickupTime);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Donation> CREATOR = new Parcelable.Creator<Donation>() {
+        @Override
+        public Donation createFromParcel(Parcel in) {
+            return new Donation(in);
+        }
+
+        @Override
+        public Donation[] newArray(int size) {
+            return new Donation[size];
+        }
+    };
 }
