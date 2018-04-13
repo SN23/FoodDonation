9801b59623c30f53fdc0811dedb966a7a9db12fe
Sukhjinder Nahal <SN23@users.noreply.github.com>
2018-04-12 22:19:49 -0400

    created DonatePresenterTest

---
+++ app/src/test/java/com/nahal/sukhjinder/fooddonation/presenter/DonatePresenterTest.java
@@ -0,0 +1,34 @@
+package com.nahal.sukhjinder.fooddonation.presenter;
+
+import android.view.View;
+
+import com.nahal.sukhjinder.fooddonation.contract.DonateActivityContract;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+public class DonatePresenterTest {
+
+    @Mock
+    private DonateActivityContract.MVPView mView;
+    @Mock
+    private View view;
+
+    private DonatePresenter mPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mPresenter = new DonatePresenter(mView);
+    }
+
+    @Test
+    public void addDonationButtonClick() {
+        mPresenter.addDonationButtonClick(view);
+        verify(mView).showAddDonationScreen();
+    }
+}
